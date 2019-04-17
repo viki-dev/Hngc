dd660b6ae82dc9d5f1a97501df5ad68a45b7468f
vigneshwaran <vigneshwaran.karthikeyan@imaginea.com>
2019-04-17 13:20:06 +0000

    conversation screen added New files added - [MyJavaService,chat] Files modified - [Main,header,profiles,pom.xml,hrdb.log,project-services.xml,app.variables.json]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -14,5 +14,37 @@ Page.onReady = function() {
      * e.g. to get value of text widget named 'username' use following script
      * 'Page.Widgets.username.datavalue'
      */
+    var chat = {
+        messages: [{
+            name: "viki",
+            message: "hi"
+        }, {
+            name: "Bot",
+            message: "hi,how are you?"
+        }, {
+            name: "viki",
+            message: "i am fine,how about you?"
+        }, {
+            name: "Bot",
+            message: "Yeah,i also fine"
+        }, {
+            name: "Bot",
+            message: "what can i do for you?"
+        }]
+    };
+    var tempname = "";
+    var obj = JSON.stringify(chat);
+    var jsob = JSON.parse(obj);
+    for (var i in jsob.messages) {
+        if (jsob.messages[i].name === tempname) {
+            document.getElementById('chat area').innerHTML += '<p>' + jsob.messages[i].message + "</p>";
+        } else {
+            document.getElementById('chat area').innerHTML += "<br>";
+            document.getElementById('chat area').innerHTML += '<p>' + jsob.messages[i].name + "</p><br><p>" + jsob.messages[i].message + "</p>";
+            tempname = jsob.messages[i].name;
+        }
+    }
 
+    Page.Widgets.Name.caption = jsob.messages.name;
+    Page.Widgets.content.caption = jsob.messages.message;
 };
